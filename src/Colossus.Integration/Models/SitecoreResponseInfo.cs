59d7a278f2a9ec58337296cc53c65500734bd50d
dmd <dmd@sitecore.net>
2016-04-18 12:07:29 +0300

    Fixed null ref exception when logout from launchpad

--- src/Colossus.Integration/Models/SitecoreResponseInfo.cs
+++ src/Colossus.Integration/Models/SitecoreResponseInfo.cs
@@ -13,76 +13,79 @@
   using Sitecore.Layouts;
 
   public class SitecoreResponseInfo : ResponseInfo
+  {
+    public Guid? ContactId { get; set; }
+
+    public VisitData VisitData { get; set; }
+
+    public Dictionary<string, object> ResponseData { get; set; }
+
+    public List<RenderingInfo> Renderings { get; set; }
+
+    public ItemInfo Item { get; set; }
+
+
+    public TestInfo Test { get; set; }
+
+    public SitecoreResponseInfo()
     {
-        public Guid? ContactId { get; set; }
-
-        public VisitData VisitData { get; set; }
-
-        public Dictionary<string, object> ResponseData { get; set; }
-
-        public List<RenderingInfo> Renderings { get; set; }
-
-        public ItemInfo Item { get; set; }
-        
-
-        public TestInfo Test { get; set; }
-
-        public SitecoreResponseInfo()
-        {
-            this.Renderings = new List<RenderingInfo>();
-        }
-
-        public static SitecoreResponseInfo FromContext()
-        {
-            if (Tracker.Current == null ) return null;
-
-            var info = new SitecoreResponseInfo
-            {
-                ContactId = Tracker.Current.Contact.TryGetValue(c => (Guid?)c.ContactId),
-                VisitData =
-                    ((IUpdatableObject)Tracker.Current.Interaction).TryGetValue(
-                        i => i.GetParts().OfType<VisitData>().FirstOrDefault())
-            };
-
-
-            if (Context.Page != null)
-            {
-                foreach (RenderingReference rendering in Context.Page.Renderings)
-                {
-                    var s = rendering.Settings;
-                    var renderingInfo = new RenderingInfo
-                    {
-                        Item = rendering.RenderingItem.TryGetValue(item => ItemInfo.FromItem(item.InnerItem)),
-                    };
-                    if (s != null)
-                    {
-                        renderingInfo.Conditions = s.Conditions;
-                        renderingInfo.DataSource = s.DataSource;
-                        renderingInfo.MultiVariateTest = s.MultiVariateTest;
-                        renderingInfo.Parameters = s.Parameters;
-                        renderingInfo.PersonalizationTest = s.PersonalizationTest;
-                        renderingInfo.Placeholder = s.Placeholder;
-                    }
-
-                    info.Renderings.Add(renderingInfo);
-                }
-            }     
-       
-            try
-            {
-                info.Test = TestInfo.FromTestCombination(Tracker.Current.CurrentPage.GetTestCombination());
-            }
-            catch (Exception ex)
-            {
-                info.ResponseData["TestError"] = ex.ToString();
-            }
-
-            if (Context.Item != null)
-            {
-                info.Item = ItemInfo.FromItem(Context.Item);
-            }
-
-            return info;
-        }
+      this.Renderings = new List<RenderingInfo>();
     }
+
+    public static SitecoreResponseInfo FromContext()
+    {
+      if (Tracker.Current == null) return null;
+
+      var info = new SitecoreResponseInfo
+      {
+        ContactId = Tracker.Current.Contact.TryGetValue(c => (Guid?)c.ContactId),
+        VisitData =
+              ((IUpdatableObject)Tracker.Current.Interaction).TryGetValue(
+                  i => i.GetParts().OfType<VisitData>().FirstOrDefault())
+      };
+
+
+      if (Context.Page != null)
+      {
+        foreach (RenderingReference rendering in Context.Page.Renderings)
+        {
+          var s = rendering.Settings;
+          var renderingInfo = new RenderingInfo
+          {
+            Item = rendering.RenderingItem.TryGetValue(item => ItemInfo.FromItem(item.InnerItem)),
+          };
+          if (s != null)
+          {
+            renderingInfo.Conditions = s.Conditions;
+            renderingInfo.DataSource = s.DataSource;
+            renderingInfo.MultiVariateTest = s.MultiVariateTest;
+            renderingInfo.Parameters = s.Parameters;
+            renderingInfo.PersonalizationTest = s.PersonalizationTest;
+            renderingInfo.Placeholder = s.Placeholder;
+          }
+
+          info.Renderings.Add(renderingInfo);
+        }
+      }
+
+      try
+      {
+        info.Test = TestInfo.FromTestCombination(Tracker.Current.CurrentPage.GetTestCombination());
+      }
+      catch (Exception ex)
+      {
+        if (info.ResponseData != null)
+        {
+          info.ResponseData["TestError"] = ex.ToString();
+        }
+      }
+
+      if (Context.Item != null)
+      {
+        info.Item = ItemInfo.FromItem(Context.Item);
+      }
+
+      return info;
+    }
+  }
 }
