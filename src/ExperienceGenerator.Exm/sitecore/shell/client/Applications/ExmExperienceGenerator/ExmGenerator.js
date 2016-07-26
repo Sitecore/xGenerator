985dc6b36eb21d283b45eee7a20320f49a3b30b2
msv <msv@sitecore.net>
2016-07-26 19:45:25 +0300

    initial Exm generator layout

---
+++ src/ExperienceGenerator.Exm/sitecore/shell/client/Applications/ExmExperienceGenerator/ExmGenerator.js
@@ -0,0 +1,17 @@
+﻿define(["sitecore", "knockout", "underscore"], function(_sc, ko, _) {
+  var exmGeneratorApp = _sc.Definitions.App.extend({
+    initialized: function () {
+      _sc.on("campaignEditor:loaded", function (editor) {
+        this.campaignEditor = editor;
+      }, this);
+
+      this.DialogLoadOnDemandPanel.refresh();
+      
+
+      this.SentCampaignsList.on("change:selectedItem", function() {
+        this.campaignEditor.showDialog();
+      },this);
+    }
+  });
+  return exmGeneratorApp;
+});
---
+++ src/ExperienceGenerator.Exm/sitecore/shell/client/Applications/ExmExperienceGenerator/dialogs/EmailCampaignEditor.js
@@ -0,0 +1,11 @@
+﻿define(["sitecore", "knockout", "underscore"], function (sc, ko, _) {
+  var dialog = _sc.Definitions.App.extend({
+    initialized: function () {
+      sc.trigger("campaignEditor:loaded", this);
+    },
+    showDialog:function(editingParameters) {
+      this.EditCampaignDialog.show();
+    }
+  });
+  return dialog;
+});
